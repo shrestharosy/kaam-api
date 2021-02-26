c140ca35ef52714695c78eb2ef90777f9a66f194
rosy <rosyshrestha028@gmail.com>
2021-02-26 14:00:57 +0545

    [DB] : Create config and connect app to mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
+import { MongooseModule } from '@nestjs/mongoose';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { KaamModule } from './kaam/kaam.module';
+import { ConfigModule } from '@nestjs/config';
+
+// MongooseModule can be used to connect to mongo database
+
+const DB_NAME = 'kaamDatabase';
+const MONGODB_URL = `mongodb+srv://username:username@cluster0.cijjo.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
 
 @Module({
-  imports: [],
+  imports: [
+    KaamModule,
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(MONGODB_URL),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
